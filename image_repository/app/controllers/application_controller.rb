2d8c33e4412f0f47415e16b19fe2c3e74914d3f1
hemant bhanot <hemant.bhanot01@gmail.com>
2021-05-02 01:34:30 -0400

    improve ui

--- image_repository/app/controllers/application_controller.rb
+++ image_repository/app/controllers/application_controller.rb
@@ -1,7 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include SessionsHelper
-
+  include ApplicationHelper
   protected
   def correct_user
     redirect_to user_path(current_user) and return if current_user.present?
